bac27cbfe1c0ea85964c8670baa92b132e1cad11
Letlhogonolo Theodore Obonye <ltobonye@gmail.com>
2021-02-01 12:20:27 +0200

    office list view complete

--- src/app/components/components-routing.module.ts
+++ src/app/components/components-routing.module.ts
@@ -1,8 +1,18 @@
-import { OfficeComponent } from './components/office-list/office/office.component';
+import { OfficeEditComponent } from './office-list/office-edit/office-edit.component';
+import { OfficeListComponent } from './office-list/office-list.component';
+import { OfficeComponent } from './office-list/office/office.component';
+
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [{ path: '', component: OfficeComponent }];
+
+const routes: Routes = [
+  { path: '', component: OfficeListComponent, children: [
+    {path: 'office', component: OfficeComponent},
+    {path: 'edit', component: OfficeEditComponent},
+    {path: 'new', component: OfficeEditComponent }
+  ] }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,14 +1,14 @@
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { faEllipsisV, faPhoneAlt, faUserFriends } from '@fortawesome/free-solid-svg-icons';
-import { OfficeComponent } from './components/office-list/office/office.component';
-import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { OfficeComponent } from './office-list/office/office.component';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ComponentsRoutingModule } from './components-routing.module';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { RouterModule } from '@angular/router';
-import { OfficeListComponent } from './components/office-list/office-list.component';
+import { OfficeListComponent } from './office-list/office-list.component';
 import { StaffEditComponent } from './staff-list/staff-edit/staff-edit.component';
 
 
